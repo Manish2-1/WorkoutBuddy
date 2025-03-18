0715423ffe3016f81680a94b8fdb1bd6bc08fe01
Manish KC <107633050+Manish2-1@users.noreply.github.com>
2025-03-18 18:20:42 +0545

    Update Home.jsx

--- Frontend/src/Routes/Home/Home.jsx
+++ Frontend/src/Routes/Home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   useEffect(() => {
     const fetchWorkouts = async () => {
       try {
-        const response = await fetch("http://localhost:4000/api/workouts", {
+        const response = await fetch("https://workoutbuddy-2x07.onrender.com/api/workouts", {
           headers: {
             Authorization: `Bearer ${user.token}`, // Send the token in the headers
           },
